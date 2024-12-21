b186cfc08b118cb2c652cea80159175cec551aff
kurehajime <xiidec@gmail.com>
2024-12-21 16:52:55 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/bingo',
-  assetPrefix: '/bingo',
+  assetPrefix: '/bingo/',
   trailingSlash: true,
 };
 
