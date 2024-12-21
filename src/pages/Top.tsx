41eef70d3cb63bff7dea45cf877f47f71a071491
kurehajime <xiidec@gmail.com>
2024-12-21 17:42:04 +0900

    ｃｓｓ

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+
 export default function Top() {
-    return <div>Top</div>;
+    return <div className="flex flex-col items-center justify-center h-screen">
+        <Link to="/create" className="bg-blue-500 text-white px-4 py-2 rounded-md">ビンゴ会場を作る</Link>
+    </div>;
 }
